44a5f5cabd3d18f833ed4e489f4d33488c58591c
Matteo Degano <matteo.degano@epitech.eu>
2023-10-10 12:24:39 +0200

    adding databases link

--- back-end/jobapp/jobapp/settings.py
+++ back-end/jobapp/jobapp/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'jobapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
-}
+} 
 
 
 # Password validation
