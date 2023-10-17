19dcad2f2c593c6b0b5549c19757857f94c036cb
mminguez <mathieu.minguez@gmail.com>
2023-10-17 13:56:14 +0200

    Remove ternary inside ternary

--- front-end/src/api/get/getUtilisateur.ts
+++ front-end/src/api/get/getUtilisateur.ts
@@ -1,6 +1,14 @@
 export const getUtilisateur = async (id? : number, token? : string) => {
-    const url = id ? `http://localhost:8000/utilisateurs/${id}` : token ? `http://localhost:8000/utilisateurs/auth_token/${token}` : `http://localhost:8000/utilisateurs/`;
-
+    let url = "";
+    if (id) {
+        url = `http://localhost:8000/utilisateurs/${id}`;
+    }
+    else if (token) {
+        url = `http://localhost:8000/utilisateurs/auth_token/${token}`;
+    }
+    else {
+        url = `http://localhost:8000/utilisateurs/`;
+    }
 
     return fetch(url, {
         method: 'GET',
