ae1ffc09a5d4c5662a85ac6a8d3fe419af1e4eb2
mminguez <mathieu.minguez@gmail.com>
2023-10-18 09:56:12 +0200

    Add useroffice - still need to be tested

--- front-end/src/components/ApplicationPage.tsx
+++ front-end/src/components/ApplicationPage.tsx
@@ -10,128 +10,142 @@ import { postApplication } from '../api/post/postApplication';
 import { postUnregister } from '../api/post/postUnregister';
 
 interface FormValues extends UtilisateurType {
-    message: string;
+  message: string;
 }
 
 const ApplicationPage = () => {
-    const { id } = useParams();
-    const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-    const [sexOptions, setSexOptions] = useState<SexType[]>([]);
-    const [sex, setSex] = useState<SexType>({ id: 0, name: '' });
-    const [user, setUser] = useState<UtilisateurType>({ firstname: '', lastname: '', email: '', phone: '', sex: sex, cv: '' });
-    const [formValues, setFormValues] = useState<FormValues>({ ...user, message: '' });
-    const token = useAuthStore((state) => state.token);
+  const { id } = useParams();
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const [sexOptions, setSexOptions] = useState<SexType[]>([]);
+  const [sex, setSex] = useState<SexType>({ id: 0, name: '' });
+  const [user, setUser] = useState<UtilisateurType>({ firstname: '', lastname: '', email: '', phone: '', sex: sex });
+  const [formValues, setFormValues] = useState<FormValues>({ ...user, message: '' });
+  const token = useAuthStore((state) => state.token);
+  const [cv, setCv] = useState<File>();
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            const fetchUser = async () => {
-               setUser(await getUtilisateur(undefined, token));
-            };
-            fetchUser();
-        };
-
-        const fetchSexOptions = async () => {
-            setSexOptions(await getSex());
-        };
-        setFormValues({ ...user, message: '' });
-        fetchSexOptions();
-    }, [isAuthenticated]);
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = {
-            firstname: formValues.firstname,
-            lastname: formValues.lastname,
-            email: formValues.email,
-            phone: formValues.phone,
-            sex_id: sex.id,
-            message: formValues.message,
-            cv: "testCV",
-          };
-          try {
-            if (isAuthenticated) {
-              const application: ApplicationType = {
-                user_id: user.id,
-                apply_date: new Date().toLocaleDateString() ,
-                advertisement_id: parseInt(id as string),
-                ...data,
-              };
-              const response = await postApplication(application);
-              if (response) {
-                console.log("Application submitted successfully");
-              } else {
-                console.error("Error submitting application");
-              }
-            } else {
-              const unregisterUser = await postUnregister(data as UnregisterType);
-              const application: ApplicationType = {
-                unregisterUser_id: unregisterUser.id,
-                apply_date: new Date().toLocaleDateString() ,
-                advertisement_id: parseInt(id as string),
-                ...data,
-              };
-              const response = await postApplication(application);
-              if (response) {
-                console.log("Application submitted successfully");
-              } else {
-                console.error("Error submitting application");
-              }
-            }
-          } catch (error) {
-            console.error(error);
-          }
+  useEffect(() => {
+    if (isAuthenticated) {
+      const fetchUser = async () => {
+        setUser(await getUtilisateur(undefined, token));
+      };
+      fetchUser();
     };
 
-    // Handle form input changes
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = event.target;
-        setFormValues({ ...formValues, [name]: value });
+    const fetchSexOptions = async () => {
+      setSexOptions(await getSex());
     };
+    setFormValues({ ...user, message: '' });
+    fetchSexOptions();
+  }, [isAuthenticated]);
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                {!isAuthenticated ?
-                    <>
-                        <label>
-                            First Name:
-                            <input type="text" name="firstname" value={formValues.firstname} onChange={handleChange} />
-                        </label>
-                        <label>
-                            Last Name:
-                            <input type="text" name="lastname" value={formValues.lastname} onChange={handleChange} />
-                        </label>
-                        <label>
-                            Email:
-                            <input type="email" name="email" value={formValues.email} onChange={handleChange} />
-                        </label>
-                        <label>
-                            Phone:
-                            <input type="tel" name="phone" value={formValues.phone} onChange={handleChange} />
-                        </label>
-                        <label>
-                            Sex:
-                            <select value={sex.id} onChange={(event) => setSex({ id: parseInt(event.target.value), name: '' })}>
-                                <option value="">Select...</option>
-                                {sexOptions.map((option) => (
-                                    <option key={option.id} value={option.id}>
-                                        {option.name}
-                                    </option>
-                                ))}
-                            </select>
-                        </label>
-                    </>
-                    : <p>Your data is imported from your profile</p>}
-                <label>
-                    Message:
-                    <textarea name="message" value={formValues.message} onChange={handleChange} />
-                </label>
-                <button type="submit">Submit</button>
-                <br />
-            </form>
-            {!isAuthenticated ? <Link to="/user" >Or... I have an account - LogIn</Link> : <></>}
-        </>
-    );
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      setCv(files[0]);
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('firstname', formValues.firstname);
+    formData.append('lastname', formValues.lastname);
+    formData.append('email', formValues.email);
+    formData.append('phone', formValues.phone);
+    formData.append('sex_id', sex.id?.toString() || '');
+    formData.append('message', formValues.message || ''); // set default value if message is not set
+    formData.append('cv', cv || '');
+    try {
+      if (isAuthenticated) {
+        const application: ApplicationType = {
+          user_id: user.id,
+          apply_date: new Date().toISOString(),
+          advertisement_id: parseInt(id as string),
+          ...formValues,
+        };
+        const response = await postApplication(application);
+        if (response) {
+          console.log("Application submitted successfully");
+        } else {
+          console.error("Error submitting application");
+        }
+      } else {
+        const unregisterUser = await postUnregister(formData);
+        const application: ApplicationType = {
+          unregisterUser_id: unregisterUser.id,
+          apply_date: new Date().toISOString(),
+          advertisement_id: parseInt(id as string),
+          ...formValues,
+        };
+        const response = await postApplication(application);
+        if (response) {
+          console.log("Application submitted successfully");
+        } else {
+          console.error("Error submitting application");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Handle form input changes
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        {!isAuthenticated ?
+          <>
+            <label>
+              First Name:
+              <input type="text" name="firstname" value={formValues.firstname} onChange={handleChange} />
+            </label>
+            <label>
+              Last Name:
+              <input type="text" name="lastname" value={formValues.lastname} onChange={handleChange} />
+            </label>
+            <label>
+              Email:
+              <input type="email" name="email" value={formValues.email} onChange={handleChange} />
+            </label>
+            <label>
+              Phone:
+              <input type="tel" name="phone" value={formValues.phone} onChange={handleChange} />
+            </label>
+            <input
+              type="file"
+              accept=".pdf"
+              name="cv"
+              id="cv"
+              onChange={handleFileChange}
+            />
+            <label>
+              Sex:
+              <select value={sex.id} onChange={(event) => setSex({ id: parseInt(event.target.value), name: '' })}>
+                <option value="">Select...</option>
+                {sexOptions.map((option) => (
+                  <option key={option.id} value={option.id}>
+                    {option.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </>
+          : <p>Your data is imported from your profile</p>}
+        <label>
+          Message:
+          <textarea name="message" value={formValues.message} onChange={handleChange} />
+        </label>
+        <button type="submit">Submit</button>
+        <br />
+      </form>
+      {!isAuthenticated ? <Link to="/user" >Or... I have an account - LogIn</Link> : <></>}
+    </>
+  );
 };
 
 export default ApplicationPage;
---
+++ front-end/src/components/UserOffice.tsx
@@ -0,0 +1,163 @@
+import { useEffect, useState } from 'react';
+import { getUtilisateur } from '../api/get/getUtilisateur';
+import { AdvertisementType } from '../typings/type';
+import { ApplicationType } from '../typings/type';
+import { useAuthStore } from "../store/authStore";
+import { UtilisateurType } from '../typings/type';
+import { getAdvertisement } from '../api/get/getAdvertisement';
+import { getApplication } from '../api/get/getApplication';
+import { putUtilisateur } from '../api/put/putUtilisateur';
+import { SexType } from '../typings/type';
+import { getSex } from '../api/get/getSex';
+
+const UserOffice = () => {
+    const token = useAuthStore((state) => state.token);
+    const [user, setUser] = useState<UtilisateurType>();
+    const [applications, setApplications] = useState<ApplicationType[]>([]);
+    const [advertisements, setAdvertisements] = useState<AdvertisementType[]>([]);
+    const [allApplications, setAllApplications] = useState<ApplicationType[]>([]);
+    const [allAdvertisements, setAllAdvertisements] = useState<AdvertisementType[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [editing, setEditing] = useState(false);
+    const [sexOptions, setSexOptions] = useState<SexType[]>([]);
+    const [sex, setSex] = useState(0);
+
+    useEffect(() => {
+        const fetchSexOptions = async () => {
+            setSexOptions(await getSex());
+        }
+        const getAllApplications = async () => {
+            const allApplications = await getApplication();
+            setAllApplications(allApplications);
+        }
+        const getAllAdvertisements = async () => {
+            const allAdvertisements = await getAdvertisement();
+            setAllAdvertisements(allAdvertisements);
+        }
+        const fetchUser = async () => {
+            const user = await getUtilisateur(undefined, token);
+            setUser(user);
+            if (user.permission.name === 'worker') {
+                await getAllApplications();
+                const userApplications = allApplications.filter(app => app.user?.id === user?.id);
+                setApplications(userApplications);
+                setLoading(false);
+            } else if (user.permission.name === 'company') {
+                await getAllAdvertisements();
+                const userAdvertisements = allAdvertisements.filter(ad => ad.company?.user?.id === user?.id);
+                setAdvertisements(userAdvertisements);
+                setLoading(false);
+            }
+        };
+        fetchSexOptions();
+        fetchUser();
+    }, [loading]);
+
+    const handleEdit = () => {
+        editing ? setEditing(false) : setEditing(true);
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const editedUser: UtilisateurType = {
+            id: user?.id ?? 0,
+            firstname: formData.get('firstname')?.toString() || user?.firstname || '',
+            lastname: formData.get('lastname')?.toString() || user?.lastname || '',
+            email: formData.get('email')?.toString() || user?.email || '',
+            phone: formData.get('phone')?.toString() || user?.phone || '',
+            permission: user?.permission || { id: 0, name: '' },
+            // sex_id: user?.sex_id,
+            // permission_id: user?.permission_id,
+        };
+        const formDataEditedUser = new FormData();
+        Object.entries(editedUser).forEach(([key, value]) => {
+            formDataEditedUser.append(key, value as string);
+        });
+        if (formData.get('cv')) {
+            formDataEditedUser.append('cv', formData.get('cv') as Blob);
+        }
+        try {
+            await putUtilisateur(editedUser.id!, formDataEditedUser); // add try-catch block
+            setUser(editedUser);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            {editing ? (
+                <form onSubmit={handleSubmit}>
+                    <h2>Edit User Information</h2>
+                    <label htmlFor="cv">CV:</label>
+                    <input type="file" name="cv" />
+                    <br />
+                    <label htmlFor="firstname">First Name:</label>
+                    <input type="text" id="firstname" name="firstname" defaultValue={user?.firstname} />
+                    <br />
+                    <label htmlFor="lastname">Last Name:</label>
+                    <input type="text" id="lastname" name="lastname" defaultValue={user?.lastname} />
+                    <br />
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" defaultValue={user?.email} />
+                    <br />
+                    <label htmlFor="phone">Phone:</label>
+                    <input type="tel" id="phone" name="phone" defaultValue={user?.phone} />
+                    <br />
+                    <label htmlFor="sex">Gender:</label>
+                        <select value={sex} onChange={(event) => setSex(parseInt(event.target.value))}>
+                            <option value="">Select...</option>
+                            {sexOptions.map((option) => (
+                                <option key={option.id} value={option.id}>
+                                    {option.name}
+                                </option>
+                            ))}
+                        </select>
+
+                     <button type="submit">Save</button>
+                </form>
+            ) : (
+                <div>
+                    <h2>User Information</h2>
+                    <p>First Name: {user?.firstname}</p>
+                    <p>Last Name: {user?.lastname}</p>
+                    <p>Email: {user?.email}</p>
+                    <p>Phone: {user?.phone}</p>
+                    <p>Gender: {user?.sex?.name}</p>
+                    <button onClick={handleEdit}>Edit Info</button> {/* added edit button */}
+                </div>
+            )}
+            {applications.length > 0 && (
+                <div>
+                    <h2>Applications</h2>
+                    {applications.map((app) => (
+                        <div key={app.id}>
+                            <p>Company: {app.advertisement?.company?.name}</p>
+                            <p>Offer: {app.advertisement?.title}</p>
+                            <p>Description: {app.advertisement?.quick_description}</p>
+                        </div>
+                    ))}
+                    <br />
+                </div>
+            )}
+            {advertisements.length > 0 && (
+                <div>
+                    <h2>Advertisements</h2>
+                    {advertisements.map((ad) => (
+                        <div key={ad.id}>
+                            <p>Title: {ad.title}</p>
+                            <p>Description: {ad.quick_description}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default UserOffice;
